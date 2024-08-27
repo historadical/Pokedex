bf4e569fa8d527450a515e0dce810457103d3cf2
Nic Laughter <nicholaslaughter@icloud.com>
2024-08-27 15:51:30 -0600

    Initial Commit

---
+++ PokedexTests/PokedexTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexTests.swift
+//  PokedexTests
+//
+//  Created by Nic on 8/27/24.
+//
+
+import Testing
+@testable import Pokedex
+
+struct PokedexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
